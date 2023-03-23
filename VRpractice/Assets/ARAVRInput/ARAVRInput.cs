d07fc80310cf3878d6391b55300ef03636fffa08
Hwangtaehun <r283@naver.com>
2023-03-23 21:18:34 +0900

    오큘러스 에셋 설치

--- VRpractice/Assets/ARAVRInput/ARAVRInput.cs
+++ VRpractice/Assets/ARAVRInput/ARAVRInput.cs
@@ -1,4 +1,5 @@
 #define PC
+//#define Oculus
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -107,6 +108,97 @@ public static class ARAVRInput
             RHand.forward = direction;
             return direction;
 #endif
+        }
+    }
+
+    //컨트롤러의 특정 버튼을 누르고 있는 동안 true를 반환
+    public static bool Get(Button virtualMask, Controller hand = Controller.RTouch)
+    {
+#if PC
+        //virtualMask에 들어온 값을 ButtonTarget 타입으로 변환해 전달한다.
+        return Input.GetButton(((ButtonTarget)virtualMask).ToString());
+#endif
+    }
+
+    //컨트롤러의 특정 버튼을 눌렀을 때 true를 반환
+    public static bool GetDown(Button virtualMask, Controller hand = Controller.RTouch)
+    {
+#if PC
+        return Input.GetButtonDown(((ButtonTarget)virtualMask).ToString());
+#endif
+    }
+
+    //컨트롤러의 특정 버튼을 눌렀다 떼었을 때 true를 반환
+    public static bool GetUp(Button virtualMask, Controller hand = Controller.RTouch)
+    {
+#if PC
+        return Input.GetButtonUp(((ButtonTarget)virtualMask).ToString());
+#endif
+    }
+
+    //컨트롤러의 Axis 입력을 반환
+    //axis: Horizontal, Vertical 값을 갖는다.
+    public static float GetAxis(string axis, Controller hand = Controller.LTouch)
+    {
+#if PC
+        return Input.GetAxis(axis);
+#endif
+    }
+
+    //컨트롤러에 진동 호출하기
+    public static void PlayVibration(Controller hand)
+    {
+    }
+
+    //카메라가 바라보는 방향을 기준으로 센터를 잡는다.
+    public static void Recenter()
+    {
+    }
+
+    public static void Recenter(Transform target, Vector3 direction)
+    {
+        target.forward = target.rotation * direction;
+    }
+
+#if PC
+    static Vector3 originScale = Vector3.one * 0.02f;
+#endif
+
+    public static void DrawCrosshair(Transform crosshair, bool isHand = true, Controller hand = Controller.RTouch)
+    {
+        Ray ray;
+
+        //컨트롤러의 위치와 방향을 이용해 레이 제작
+        if(isHand)
+        {
+#if PC
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+#endif
+        }
+        else
+        {
+            //카메라를 기준으로 화면의 정중앙으로 레일르 제작
+            ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+        }
+        //눈에 안 보이는 Plane을 만든다.
+        Plane plane = new Plane(Vector3.up, 0);
+        float distance = 0;
+
+        //plane을 이용해 ray를 쏜다.
+        if(plane.Raycast(ray, out distance))
+        {
+            //레이의 GetPoint 함수를 이용해 충돌 지점의 위치를 가져온다.
+            crosshair.position = ray.GetPoint(distance);
+            crosshair.forward = -Camera.main.transform.forward;
+            //크로스헤어의 크기를 최소 기본 크기에서 거리에 따라 더 커지도록 한다.
+            crosshair.localScale = originScale * Mathf.Max(1, distance);
+        }
+        else
+        {
+            crosshair.position = ray.origin + ray.direction * 100;
+            crosshair.forward = -Camera.main.transform.forward;
+            distance = (crosshair.position - ray.origin).magnitude;
+            crosshair.localScale = originScale * Mathf.Max(1, distance);
         }
     }
 }
